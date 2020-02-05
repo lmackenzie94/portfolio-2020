6790166d685c801ae087ab7f45c4d77c2dc419a9
Luke MacKenzie <luke.mackenzie@campjefferson.com>
2020-02-04 22:58:00 -0500

    cleanup

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -5,22 +5,12 @@ exports.onCreatePage = ({page, actions}) => {
   const {createPage, deletePage} = actions
   deletePage(page)
   createPage(page)
-  // createLocalePage(page, createPage);
 }
 
 exports.createPages = async ({actions, graphql, reporter}) => {
   const {createPage} = actions
   const result = await graphql(`
     {
-      allSanityProject {
-        edges {
-          node {
-            slug {
-              current
-            }
-          }
-        }
-      }
       allSanityPost {
         edges {
           node {
@@ -37,20 +27,7 @@ exports.createPages = async ({actions, graphql, reporter}) => {
   if (result.errors) throw result.errors
 
   // returns an array of just our nodes
-  const projects = result.data.allSanityProject.edges.map(({node}) => node)
   const posts = result.data.allSanityPost.edges.map(({node}) => node)
-
-  projects.forEach(project => {
-    let page = {
-      path: `/${project.slug.current}`,
-      component: path.resolve('./src/templates/project.js'),
-      // anything passed to 'context' becomes available to the graphql query in project.js template
-      context: {
-        slug: project.slug.current,
-      },
-    }
-    createPage(page)
-  })
 
   posts.forEach(post => {
     let page = {
