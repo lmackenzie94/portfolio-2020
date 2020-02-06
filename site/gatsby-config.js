336f65b83f9053545d9ba162d377f8e2490686a1
Luke MacKenzie <luke.mackenzie@campjefferson.com>
2020-02-06 11:11:06 -0500

    changed ga analytics id

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: 'UA-156009404-1',
+        trackingId: 'UA-157951713-1',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
       },
