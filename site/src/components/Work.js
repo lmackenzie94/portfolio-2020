e2d7fbfcc72664c02d4ff0be9e1eb102630ca008
Luke <mackenzieluke94@gmail.com>
2020-05-11 17:13:25 -0400

    adjusted animations

--- site/src/components/Work.js
+++ site/src/components/Work.js
@@ -7,29 +7,37 @@ import AccordionContent from './Accordion/Content'
 import AccordionButton from './Accordion/Button'
 import AccordionContainer from './Accordion/Container'
 import './Accordion/accordion.css'
+import {motion} from 'framer-motion'
 
 const Work = ({projects}) => {
   const projectArray = projects.edges.map(({node: project}) => project)
 
   return (
     <Wrapper id="Work">
+      {/* figure out how to animate Section with Framer */}
       <Section>
-        <h2 sx={{variant: `text.subheading`}}>Work</h2>
-        <Accordion
-          items={projectArray}
-          ItemRenderer={AccordionContent}
-          ButtonComponent={AccordionButton}
-          Container={AccordionContainer}
-          sx={{
-            '& [data-accordion-wrapper-inner]': {
-              p: 0,
-              px: [3, 3, 4],
-              color: 'ref',
-            },
-          }}
-          // labelkey={`title`}
-          // allowMultipleOpen
-        />
+        <motion.div
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          transition={{delay: 1}}
+        >
+          <h2 sx={{variant: `text.subheading`}}>Work</h2>
+          <Accordion
+            items={projectArray}
+            ItemRenderer={AccordionContent}
+            ButtonComponent={AccordionButton}
+            Container={AccordionContainer}
+            sx={{
+              '& [data-accordion-wrapper-inner]': {
+                p: 0,
+                px: [3, 3, 4],
+                color: 'ref',
+              },
+            }}
+            // labelkey={`title`}
+            // allowMultipleOpen
+          />
+        </motion.div>
       </Section>
     </Wrapper>
   )
