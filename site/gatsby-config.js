39fbe90218123ea232a99d0b5604a6f1fb70f224
Luke MacKenzie <mackenzieluke94@gmail.com>
2021-06-02 21:02:47 -0400

    add google tag manager

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -56,35 +56,33 @@ module.exports = {
         },
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
-    //   options: {
-    //     fonts: [
-    //       {
-    //         family: `Heebo`,
-    //         variants: [`900`, `700`],
-    //       },
-    //       {
-    //         family: `Lora`,
-    //         variants: [`400`],
-    //       },
-    //       {
-    //         family: `Noto Sans`,
-    //         variants: [`400`, `700`],
-    //       },
-    //     ],
-    //   },
-    // },
-    // `gatsby-plugin-preact`,
-    // {
-    //   resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
-    //   options: {
-    //     // production: true,
-    //     defaultSizes: `gzip`,
-    //   },
-    // },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-M6KZNS8',
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        // includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: {platform: 'gatsby'},
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+      },
+    },
   ],
 }
